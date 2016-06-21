0e5ccc47841064c00f283b5f7449988a6926d4cb
Gustavo Barron <cicloid@42fu.com>
2016-06-21 12:49:47 -0500

    serve files from public

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
-    const indexPath = path.join(__dirname, '/public/index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.use(publicPath)
 
     return app
   }
