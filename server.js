fecad0ff69f9530b162b61937edcd0d308e46a38
Gustavo Barron <cicloid@42fu.com>
2016-06-21 12:29:19 -0500

    File built

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
-    const indexPath = path.join(__dirname, 'index.html')
+    const indexPath = path.join(__dirname, '/public/index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
 
     app.use('/public', publicPath)
